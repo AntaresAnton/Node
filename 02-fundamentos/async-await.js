1f007d9d83d1f465b0ecaa98e863415fb5fb82e7
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-06-11 22:08:43 -0400

    Update async-await.js

--- 02-fundamentos/async-await.js
+++ 02-fundamentos/async-await.js
@@ -30,6 +30,7 @@ const salarios = [
     }
 
 ];
+const id = 1;
 
 const getEmpleado = (id) => {
     const promesa = new Promise((resolve, reject) => {
@@ -45,13 +46,10 @@ const getEmpleado = (id) => {
     return promesa;
 }
 
-const id = 1;
+
 getEmpleado(id)
     .then(empleado => console.log(empleado))
     .catch(err => console.log(err));
-
-
-
 
 
 const getEmpleado2 = (id) => {
@@ -79,3 +77,46 @@ const getSalario = (id) => {
             : reject(`salario no existente para id ${id}`)
     });
 }
+
+
+// async await son terminos aplicados para las promesas.
+// el await le dice, js espera hasta que tengas la respuesta de la promesa. una vez tengas el dato, lo ejecutas
+// el await debe estar si o si dentro de una función async
+
+// async es transformar la función para que regrese una promesa
+
+// parte 1 - opción 1 sin manejador de error (without handler)
+// const getInfoUsuario = async ()=>{
+//     const empleado = await getEmpleado(id);
+//     const salario = await getSalario(id);
+//     return `El salario del empleado: ${empleado} es de ${salario}`
+// }
+
+// getInfoUsuario(id).then(msg=>console.log(msg))
+
+
+// con el manejador de errores. acá se le aplica un try catch, eso es debido a que se necesita un catch para manejar el error en caso de inresar un parámetro incorrecto o que no esté
+
+// para editar los parámetros, hay que ir a la linea 33 y cambiar el número
+const getInfoUsuario = async ()=>{
+    try {
+        const empleado = await getEmpleado(id);
+        const salario = await getSalario(id);
+        return `El salario del empleado: ${empleado} es de ${salario}`
+    } catch (error) {
+        throw error;
+        
+    }
+}
+
+getInfoUsuario(id)
+.then(msg=>{
+    console.log('TODO BIEN!')
+    console.log(msg)
+}
+    
+    )
+.catch(err => {
+    console.log('TODO MAL')
+    console.log(err)
+});
