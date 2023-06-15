7b3cc5e878bb730719a3d51a1a7a975458e9e3cb
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-06-14 20:19:03 -0400

    Create app.js

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,15 @@
+console.clear();
+// console.log('Inicio de programa');
+// para llamar a la libreria de los colores
+require('colors');
+
+// haremos una constante main
+
+// breve reseña de las funciones asíncronas (en base a lo que he leido):
+// las funciones asíncronas son las que están a la espera de que lleguen datos. uno puede interpretar por ejemplo que discord maneja tanto funciones sincronas y asincronas. las sincronas son por ejemplo cuanso uno está hablando en directo con una persona por voz. y la asíncrona es cuando te manda un mensaje por texto (y se te olvida abrirlo o no lo abres jaja)
+const main=async()=>{
+    console.log('hola mundo');
+
+}
+
+main();
