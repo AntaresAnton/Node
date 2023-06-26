fc1a847b34b53944dd96c17c39caf5759d7e432f
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-06-25 20:54:16 -0400

    76

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,11 +1,16 @@
+// este paquete es para modificar las variables de entorno, permite alojar tokens
+require('dotenv').config();
 // Para llamar a las funciones de los archivos que se encuentran en las otras carpetas :)
 const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer.js');
 const { Busquedas } = require('./models/busquedas.js');
 
+// este console log despliega el token de mapbox que está alojado en el archivo .env y que dotenv permite alojar
+// console.log(process.env.MAPBOX_KEY);
+
 // función asíncrona main
 const main = async () => {
     // con la constante de abajo se llama a la clase que está ubicada en el archivo de modelo
-    const busquedas = new Busquedas
+    const busquedas = new Busquedas;
     let opt;
 
     // para el menú, se hace el do while, esto para que el menú pueda ser interactivo y restringir algunas cosas (condicionantes)
@@ -19,6 +24,7 @@ const main = async () => {
                     // mostrar mensajes
                     const lugar = await leerInput('Ciudad: ')
                     // console.log(lugar);
+                    // con el await de abajo, se llama a la clase "busquedas" (linea 8 del cod). esta llama a las busquedas del archivo "busquedas.js". va directo a la clase definida, y llega al "async ciudad" para obtener el resultado
                     await busquedas.ciudad(lugar);
                     // buscar los lugares
 
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -9,13 +9,30 @@ class Busquedas {
         // TODO: Leer DB en caso que exista
     }
 
+    get paramsMapbox(){
+        return{
+            'access_token': process.env.MAPBOX_KEY, //token de acceso generado en mapbox
+            'limit': 5, //limite de resultados de busqueda
+            'language': 'es' //idioma de los resultados
+        } 
+    }
+
     // función asíncrona para almacenar el lugar. este al ser string se le añaden las comillas simples
     async ciudad(lugar = ''){
 
         try {
             // peticion HTTP
         // console.log('Ciudad', lugar);
-        const resp = await axios.get('https://reqres.in/api/users?page=2')
+
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();
+
+
+        // const resp = await axios.get('')
         console.log(resp.data);
         } catch (error) {
             console.log('No se encontró nada')
