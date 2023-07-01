4f0fc30590a4784c46f7ed2a1467396c1b01b92f
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-06-30 21:19:56 -0400

    89

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+
+
+// servir contenido estático
+app.use(express.static('public'));
+
 // los app get con express sirven para realizar "rutas", esto quiere decir que podemos condicionar las rutas respectivas para nuestra app. como se ve en los ejemplos de abajo, podemos ir personalizando cada enlace, al final hay un comodín ('*') asterisco, que sirve para dejar un mensaje en cualquier ruta que no exista.
-app.get('/', function (req, res) {
-    res.send('Home Page')
-  });
 
 app.get('/hola-mundo', function (req, res) {
   res.send('Hola Mundo en su respectiva ruta')
